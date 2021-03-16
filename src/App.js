a935ed75c743e0b29577a59651890db420deb55c
Phong Luu <huynhpluu@gmail.com>
2021-03-16 20:19:44 +0700

    added: nav

--- src/App.js
+++ src/App.js
@@ -6,35 +6,86 @@ import {
   Redirect,
   useLocation,
   useHistory,
+  useRouteMatch,
 } from "react-router-dom";
 import { useAuth } from "./context/auth-context";
 import { useAsync } from "utils/hooks";
 import { client } from "utils/api-client";
 
 function App() {
+  const { user } = useAuth();
   return (
     <>
       <AuthButton />
+      <div className="flex items-center gap-2 absolute top-5 right-5">
+        {user ? (
+          <>
+            <div>{user.email}</div>
+            <button className="p-2 bg-gray-100 rounded-sm">Logout</button>
+          </>
+        ) : (
+          <button className="p-2 bg-gray-100 rounded-sm">Login</button>
+        )}
+      </div>
+      <div
+        className="w-full max-w-4xl grid gap-2 my-0 mx-auto py-4 px-2"
+        style={{ gridTemplateColumns: "1fr 3fr" }}
+      >
+        <Nav />
+        <main>
+          <AppRoutes />
+        </main>
+      </div>
+      <AppRoutes />
+    </>
+  );
+}
+
+function Nav() {
+  return (
+    <nav className="sticky border-gray-900 rounded-sm px-2 py-4">
       <ul>
         <li>
-          <Link to="/public">Public Page</Link>
+          <NavLink to="/list">Reading List</NavLink>
         </li>
         <li>
-          <Link to="/protected">Protected Page</Link>
+          <NavLink to="/finished">Finised Books</NavLink>
+        </li>
+        <li>
+          <NavLink to="/discover">Discover</NavLink>
         </li>
       </ul>
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/public">
-          <PublicPage />
-        </Route>
-        <PrivateRoute path="/protected">
-          <ProtectedPage />
-        </PrivateRoute>
-      </Switch>
-    </>
+    </nav>
+  );
+}
+
+function NavLink(props) {
+  const match = useRouteMatch(props.to);
+  const matchCSS = "bg-gray-200";
+  return (
+    <Link
+      className={`block px-2 pt-4 pb-2 my-1 mx-0 w-full h-full rounded-sm hover:text-indigo-400 hover:bg-gray-200 ${
+        match ? matchCSS : ""
+      }`}
+      style={{ borderLeft: "5px solid transparent" }}
+      {...props}
+    />
+  );
+}
+
+function AppRoutes() {
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route path="/public">
+        <PublicPage />
+      </Route>
+      <PrivateRoute path="/protected">
+        <ProtectedPage />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
