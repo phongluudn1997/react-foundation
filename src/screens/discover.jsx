ff0f4a18cb7829dae35b4e14ca759122ac599520
Phong Luu <huynhpluu@gmail.com>
2021-03-17 10:03:22 +0700

    added: UI for discover book

--- src/screens/discover.jsx
+++ src/screens/discover.jsx
@@ -1,21 +1,19 @@
 import * as React from "react";
 import { Input, Spinner } from "@chakra-ui/react";
 import { useAsync } from "utils/hooks";
+import { client } from "utils/api-client";
+import { Link } from "react-router-dom";
 
 function Discover() {
-  const {
-    data: books,
-    isLoading,
-    isError,
-    error,
-    execute,
-    isSuccess,
-  } = useAsync();
+  const { data, isLoading, isError, error, execute, isSuccess } = useAsync();
   const [query, setQuery] = React.useState("");
   const firstRender = React.useRef(true);
 
   React.useEffect(() => {
-    if (!firstRender.current) {
+    if (firstRender.current) {
+      execute(client("/books/discover"));
+    } else {
+      execute(client(`/books?search=${query}`));
     }
   }, [execute, query]);
 
@@ -27,6 +25,8 @@ function Discover() {
     e.preventDefault();
     setQuery(e.target.elements.search.value);
   };
+
+  console.log(data);
 
   return (
     <div>
@@ -44,15 +44,57 @@ function Discover() {
       <div>
         {isLoading ? <Spinner /> : null}
         {isSuccess ? (
-          books.length ? (
-            <ul>
-              books.map(book => <li>Book</li>)
+          data.data.books.length ? (
+            <ul className="space-y-4">
+              {data.data.books.map((book) => (
+                <li key={book._id}>
+                  <BookRow book={book} />
+                </li>
+              ))}
             </ul>
-          ) : null
+          ) : (
+            "No books"
+          )
         ) : null}
       </div>
     </div>
   );
+}
+
+function BookRow({ book }) {
+  const { title, author, coverImageUrl, publisher, synopsis } = book;
+
+  return (
+    <div className="flex items-center justify-end">
+      <Link
+        className="grid gap-5 border border-gray-200 rounded-sm p-5"
+        style={{ gridTemplateColumns: "140px 1fr" }}
+      >
+        <div>
+          <img src={coverImageUrl} alt="Book" />
+        </div>
+        <div>
+          <div className="flex justify-between">
+            <div className="flex-1">
+              <h2>{title}</h2>
+            </div>
+            <div className="ml-2">
+              <div className="italic text-sm">{author}</div>
+              <small>{publisher}</small>
+            </div>
+          </div>
+          <small className="break-words">{synopsis.substring(0, 500)}...</small>
+        </div>
+      </Link>
+      <div>
+        <StatusButton />
+      </div>
+    </div>
+  );
+}
+
+function StatusButton() {
+  return <div></div>;
 }
 
 export { Discover };
