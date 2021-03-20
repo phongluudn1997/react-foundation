a3b776a9faac90d4d404e98c688db56bea762877
Phong Luu <huynhpluu@gmail.com>
2021-03-20 17:10:53 +0700

    added: book screen

--- src/screens/book.jsx
+++ src/screens/book.jsx
@@ -16,7 +16,26 @@ function Book() {
     return "...Loading";
   }
 
-  return <div></div>;
+  return (
+    <div className="grid gap-4" style={{ gridTemplateColumns: "1fr 2fr" }}>
+      <img src={book.coverImageUrl} alt="book cover" />
+
+      <div>
+        <div className="flex">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">{book.title}</h1>
+            <div>
+              <i>{book.author}</i>
+              <span className="mx-2">|</span>
+              <i>{book.publisher}</i>
+            </div>
+          </div>
+        </div>
+        <br />
+        <p>{book.synopsis}</p>
+      </div>
+    </div>
+  );
 }
 
 export { Book };
